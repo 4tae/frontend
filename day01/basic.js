187a31a553c1d68f2eda4778e4bbe970a1aa3921
4tae <twp031@gmail.com>
2023-01-25 20:05:35 +0900

    첫번째 커밋

---
+++ day01/basic.js
@@ -0,0 +1,8 @@
+document.querySelector('p').onclick = function () {
+    document.querySelector('p').style.color = 'red';
+}; //한 줄의 코드가 끝나서 붙은 세미콜론
+
+function a() {
+console.log('안녕');
+} //중괄호 뒤에는 세미콜론 안붙인다
+
